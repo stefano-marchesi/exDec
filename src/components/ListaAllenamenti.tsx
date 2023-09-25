d990701f15b560f82ac23dd28c8ebe8fca61c304
stefano-marchesi <stefano.marchesi1984@gmail.com>
2023-09-25 18:22:07 +0200

    aggiunta lista allenamenti

--- src/components/ListaAllenamenti.tsx
+++ src/components/ListaAllenamenti.tsx
@@ -1,10 +1,36 @@
+import { useSelector } from "react-redux"
+import { selectAllenamento } from "../redux/allenamentiReducer"
+
 type ListaAllenamentiProps = {
   idParte:number
 }
-export const ListaAllenamenti = (props : ListaAllenamentiProps)=>{
 
+const formattaData = (data : Date)=>{
+  return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`
+}
+
+export const ListaAllenamenti = (props : ListaAllenamentiProps)=>{
+  const allenamenti = useSelector(selectAllenamento).filter((elem)=>{
+    return elem.idParte===props.idParte
+  })
   return(
-    <div>Lista allenamenti {props.idParte}</div>
+    <div>
+     <div> Lista allenamenti </div> 
+      <div>
+        {allenamenti.map((allenamento)=>{
+          return (
+            <div>
+            <div>
+                intensita: {allenamento.intensita}
+            </div>
+            <div>
+                data: {formattaData(new Date(allenamento.data))}
+            </div>
+            </div>
+          )
+        })}
+      </div>
+     </div>
   )
 
 
