04a8c04bd44676cae90891d868905aa219a4a2b7
EliSan86 <eli86san@gmail.com>
2023-09-27 22:09:46 +0200

    immagine che non riesce ad essere un bg

--- src/pagine/Main.tsx
+++ src/pagine/Main.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom"
 import './../index.css'
-import logo from '../assets/photo-1503023345310-bd7c1de61c7d.jpeg';
+import sfondo from '../assets/abstr01.png'
 export const Main = () => {
   return (
-  
-    <div> 
-      <div className="flex justify-evenly bg-gray-950 p-2 "> 
-        <div className=" bg-gray-700 rounded-full shadow-md text-orange-500 font-bold w-10 h-10  text-xs items-center inline-flex justify-center ">Home</div>
-        <div className=" bg-gray-700 rounded-full shadow-md text-orange-500 font-bold w-10 h-10  text-xs items-center inline-flex justify-center ">Back</div>
-        <div className=" bg-gray-700 rounded-full shadow-md text-orange-500 font-bold w-10 h-10  text-xs items-center inline-flex justify-center ">Boh</div>
+
+    <div>
+      <div className=""> < img src={sfondo}></img>
+        <div className="flex justify-evenly p-2 ">
+          <div className=" bg-gray-700 rounded-full shadow-md text-orange-500 font-bold w-10 h-10  text-xs items-center inline-flex justify-center ">Back</div>
+          <div className=" bg-gray-700 rounded-full shadow-md text-orange-500 font-bold w-10 h-10  text-xs items-center inline-flex justify-center ">Home</div>
+          <div className=" bg-gray-700 rounded-full shadow-md text-orange-500 font-bold w-10 h-10  text-lg items-center inline-flex justify-center font-serif ">i</div>
+        </div>
       </div>
       <Outlet />
       <div className="bg-gray-950 h-screen italic text-orange-600 text-center font-light p-2">Ricordati di bere tanta acqua!</div>
