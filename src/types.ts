4655287d2fb070d52561a2e7acda4e939ac25a65
stefano-marchesi <stefano.marchesi1984@gmail.com>
2023-11-01 11:23:37 +0100

    ok

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,17 @@
 export type user = {
   nome:string
 }
+
+
+export type Allenamento = {
+  data: number,
+  intensita: number,
+  idParte: number
+}
+
+export type Parte = {
+	nome: string,
+	stress: number,
+	categoria: string,
+	id:number
+}
