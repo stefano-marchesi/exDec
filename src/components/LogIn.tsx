bd43b5a2af2f51d55598465011ef7b315f86a33b
stefano-marchesi <stefano.marchesi1984@gmail.com>
2023-11-01 09:38:57 +0100

    ok

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -23,10 +23,10 @@ export const LogIn = () => {
   }
 
   return (
-    <div className="bg-gradient-to-r from-gray-900 via-gray-700 to-gray-800 pt-10 pb-10 ">log in
+    <div className="bg-[url('assets/abs.png')]  pt-10 pb-10 ">
       <div className="flex flex-col items-center">
-        <input className="border-2 w-2/3 border-black bg-slate-600 text-gray-300 rounded" value={credential.mail} onChange={handleChangeMail} />
-        <input className="border-2 w-2/3 border-black bg-slate-600 text-gray-300 mt-1 rounded" value={credential.pass} onChange={handleChangePassword} />
+        <input className="border-2 border-black bg-slate-600 text-gray-300 rounded" value={credential.mail} onChange={handleChangeMail} />
+        <input className="border-2 border-black bg-slate-600 text-gray-300 mt-1 rounded" value={credential.pass} onChange={handleChangePassword} />
 
         <button className="btn-primary" onClick={handleClick}>AVANTI</button>
       </div>
--- src/components/LoginManager.tsx
+++ src/components/LoginManager.tsx
@@ -9,14 +9,18 @@ export const LoginManager = () => {
         <div className=" text-4xl font-bold text-orange-500 drop-shadow-md pb-20"><p className=" mt-32 text-center">Hey There!</p></div>
         <div className="flex flex-col items-center"><div className=" text-white"><p>Hai già un account?</p></div>
           <button className="btn-primary" onClick={() => changeLogIn(true)}>LOG IN</button>
+
+      {logIn
+        ? <LogIn />
+        : ''}
         </div>
         <div className="flex flex-col items-center mt-5"><div className=" text-white"><p>Sei un nuovo utente?</p></div>
           <button className="btn-primary" onClick={() => changeLogIn(false)}>SIGN IN</button>
+      {logIn
+        ?'' 
+        : <SignIn />}
         </div>
       </div>
-      {logIn
-        ? <LogIn />
-        : <SignIn />}
     </div>
   )
 }
